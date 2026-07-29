5106b81bcecce00656acff22dd2234a6f6962473
VallabhA <vallabh.ansingkar@porsche-nardo.com>
2026-07-29 08:19:06 +0200

    The return expected a full sentence

--- cpp/vehicle-purchase/vehicle_purchase.cpp
+++ cpp/vehicle-purchase/vehicle_purchase.cpp
@@ -13,7 +13,7 @@ bool needs_license(std::string kind) {
 // vehicle that comes first in lexicographical order.
 std::string choose_vehicle(std::string option1, std::string option2) {
     // TODO: Return the final decision in a sentence.
-    return (option1<option2)? option1: option2;
+    return (option1<option2)? option1+" is clearly the better choice.":option2+" is clearly the better choice.";
 }
 
 // calculate_resell_price calculates how much a vehicle can resell for at a
